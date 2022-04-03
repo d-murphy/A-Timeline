78076513e99ab58a19a090b01e10db58ff98df17
d-murphy <murphy.dan.s@gmail.com>
2022-04-03 15:19:24 -0400

    added event divs

--- src/Data.tsx
+++ src/Data.tsx
@@ -2,8 +2,8 @@ const TIMELINE_DATA_SOURCE = 'mock'
 
 type TimelineEvent = {
     text: string, 
-    position: number,
-    side: string
+    date: Date, 
+    eventMarker?: string
 }
 
 const getTimeline = function (id:number) : TimelineEvent[] {
@@ -24,26 +24,38 @@ function getMockData(id:number) : TimelineEvent[] {
     const timelines: timelineLookup = {
         1: [
             {
-                side: 'left',
-                position: 1,
-                text: 'here is the first event in the timeline'
+                text: 'here is the first event in the timeline', 
+                date: new Date('1/4/2015')
             }, 
             {
-                side: 'right', 
-                position: 2,
-                text: 'here is the second event in the timeline'
+                text: 'here is the second event in the timeline', 
+                date: new Date('3/4/2015')
+            }, 
+            {
+                text: 'here is the third event in the timeline', 
+                date: new Date('3/4/2016')
+            }, 
+            {
+                text: 'another event', 
+                date: new Date('3/10/2016')
+            }, 
+            {
+                text: 'The FINAL event', 
+                date: new Date('4/10/2016')
             }
         ],
         2: [
             {
-                side: 'right', 
-                position: 1,
-                text: 'In the beginning'
+                text: 'In the beginning', 
+                date: new Date('1/4/2015')
             }, 
             {
-                side: 'right', 
-                position: 2, 
-                text: 'This is the second thing to take place.'
+                text: 'This is the second thing to take place.', 
+                date: new Date('3/4/2015')
+            }, 
+            {
+                text: 'This is the second thing to take place.', 
+                date: new Date('3/4/2016')
             }
         ]
     }
--- src/Timeline.tsx
+++ src/Timeline.tsx
@@ -7,10 +7,63 @@ type TimelineProps = {
 
 function Timeline(props:TimelineProps) {
     const timelineData = getTimeline(props.id);
-    const events = timelineData.map(el => {
-        return <li>Text: {el.text}; will appear on the {el.side}</li>
+    const groupByYear = manipTimelineData(timelineData); 
+    console.log('checking1', groupByYear)
+    console.log('checking2', Object.values(groupByYear))
+    const events = Object.values(groupByYear).map((year:YearGroup) => {
+       
+      return  <div className='year-section'>
+                <div className='year-heading'>
+                  {year.year}
+                </div>
+                <div className='years-events'>
+                  {
+                    year.events.map( (el:TimelineEvent, ind:number) => {
+                      let eventSectionNameWithSide:string = ind % 2 == 0 ? 'event-section-right' : 'event-section-left'; 
+                      return  <div key={`${year.year}-${ind}`} className={eventSectionNameWithSide}>
+                                <div className='event-marker'>{el.eventMarker ? <img className='event-marker-image' src={el.eventMarker}/> : <div className='event-marker-no-image'></div>}</div>
+                                <div className='event-header'>{el.date.getMonth()+1}</div>
+                                <div className='event-body'>{el.text}</div>
+                              </div>
+                  })}
+                </div>
+              </div>
+
+//        return <li key={ind}>Text: {el.text}; happened on  {el.date.getMonth()+1}/{el.date.getFullYear()}</li>
     })
     return <ul>{events}</ul>
 }
 
 export default Timeline
+
+type TimelineEvent = {
+  text: string, 
+  date: Date, 
+  eventMarker?: string
+}
+
+type GroupByYear = {
+  [index: number] : YearGroup;
+}
+
+type YearGroup = {
+  year: number,
+  events: TimelineEvent[]
+}
+
+
+function manipTimelineData(timelineData: TimelineEvent[]): GroupByYear{
+  timelineData = timelineData.sort((a,b) => a.date < b.date ? -1 : 1)
+  const groupByYear:GroupByYear = {}; 
+  timelineData.forEach(el => {
+    if(!groupByYear[el.date.getFullYear()]){
+      groupByYear[el.date.getFullYear()] = {
+        year: el.date.getFullYear(),
+        events: [el]
+      }
+    } else {
+      groupByYear[el.date.getFullYear()].events.push(el)
+    }
+  })
+  return groupByYear;
+}
