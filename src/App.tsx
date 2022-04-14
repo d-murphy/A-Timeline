8370764771a75317370b085de1568910db7a52c4
d-murphy <murphy.dan.s@gmail.com>
2022-04-13 22:16:49 -0400

    added footer, before changing css

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Timeline from "./Timeline";
+import Footer from "./Footer"
 
 export default function App() {
     return (
         <div>
             <Timeline id={1} />
+            <Footer />
         </div>
     );
 }
---
+++ src/Footer.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Banner = function(){
+    return (
+    <div className="footer ">
+        <div className="footer-logo">
+            <div>footer-logo</div>
+            <a href="/timegeneratorHome">Timeline Generator Home</a>
+        </div>
+        <div className="footer-header">
+            <div>footer-header</div>
+        </div>
+        <div className="footer-header">
+            <div>footer-header</div>
+        </div>
+        <div className="footer-side5">
+            <div>footer-side5</div>
+        </div>
+        <div className="footer-side4">
+            <div>footer-side4</div>
+        </div>
+        <div className="footer-side3">
+            <div>footer-side3</div>
+        </div>
+        <div className="footer-side2">
+            <div>footer-side2</div>
+        </div>
+        <div className="footer-side1">
+            <div>footer-side1</div>
+        </div>
+    </div>
+    )
+}
+
+export default Banner;
